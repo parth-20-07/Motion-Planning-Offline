e6db980953cffa11b680a372959188b2903dcebf
Parth Patel <parth.pmech@gmail.com>
2023-01-29 14:54:50 -0500

    Add BFS Algorithm in Assignment 1

---
+++ HW 1: BFS and DFS Algorithm/search.py
@@ -0,0 +1,213 @@
+import queue
+# Basic searching algorithms
+
+# Class for each node in the grid
+
+
+class Node:
+    def __init__(self, row, col, is_obs, h):
+        self.row = row        # coordinate
+        self.col = col        # coordinate
+        self.is_obs = is_obs  # obstacle?
+        self.cost = None      # total cost (depend on the algorithm)
+        self.parent = None    # previous node
+
+
+right = [0, +1]
+down = [+1, 0]
+left = [0, -1]
+up = [-1, 0]
+
+
+def is_left_empty(grid, y, x):
+    if x == 0:
+        return False
+    else:
+        if grid[y][x-1] == 0:
+            return True
+        else:
+            return False
+
+
+def is_right_empty(grid, y, x):
+    grid_height = len(grid)-1
+    grid_width = len(grid[0]) - 1
+    if x == grid_width:
+        return False
+    else:
+        if grid[y][x+1] == 0:
+            return True
+        else:
+            return False
+
+
+def is_up_empty(grid, y, x):
+    if y == 0:
+        return False
+    else:
+        if grid[y-1][x] == 0:
+            return True
+        else:
+            return False
+
+
+def is_down_empty(grid, y, x):
+    grid_height = len(grid)-1
+    grid_width = len(grid[0]) - 1
+    if y == grid_height:
+        return False
+    else:
+        if grid[y+1][x] == 0:
+            return True
+        else:
+            return False
+
+
+def clear_grid(grid):
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if (grid[y][x] == 2):
+                grid[y][x] = 0
+
+
+def list_path(start, steps, path, final_path):
+    print(f"Steps for BFS: {final_path}")
+    y, x = start
+    for step in final_path:
+        if (step == 'D'):
+            y = y+1
+            steps += 1
+            path.append((y, x))
+        elif (step == 'R'):
+            x = x+1
+            steps += 1
+            path.append((y, x))
+        elif (step == 'U'):
+            y = y-1
+            steps += 1
+            path.append((y, x))
+        elif (step == 'L'):
+            x = x-1
+            steps += 1
+            path.append((y, x))
+    return steps, path
+
+
+def bfs(grid, start, goal):
+    '''Return a path found by BFS alogirhm
+       and the number of steps it takes to find it.
+
+    arguments:
+    grid - A nested list with datatype int. 0 represents free space while 1 is obstacle.
+           e.g. a 3x3 2D map: [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+    start - The start node in the map. e.g. [0, 0]
+    goal -  The goal node in the map. e.g. [2, 2]
+
+    return:
+    path -  A nested list that represents coordinates of each step (including start and goal node),
+            with data type int. e.g. [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2]]
+    steps - Number of steps it takes to find the final solution,
+            i.e. the number of nodes visited before finding a path (including start and goal node)
+
+    >>> from main import load_map
+    >>> grid, start, goal = load_map('test_map.csv')
+    >>> bfs_path, bfs_steps = bfs(grid, start, goal)
+    It takes 10 steps to find a path using BFS
+    >>> bfs_path
+    [[0, 0], [1, 0], [2, 0], [3, 0], [3, 1]]
+    '''
+    ### YOUR CODE HERE ###
+    path = []
+    steps = 0
+    found = False
+    x_queue = queue.Queue()
+    y_queue = queue.Queue()
+    p_queue = queue.Queue()
+    y, x = start
+    grid[y][x] = 2
+    x_queue.put(x)
+    y_queue.put(y)
+    p_queue.put("")
+    final_path = ''
+    while not x_queue.empty():
+        x = x_queue.get()
+        y = y_queue.get()
+        path_search = p_queue.get()
+
+        if ((y == goal[0]) and (x == goal[1])):
+            final_path = path_search
+            found = True
+            break
+
+        if (is_down_empty(grid, y, x)):
+            y_queue.put(y+1)
+            x_queue.put(x)
+            p_queue.put(path_search+'D')
+            grid[y+1][x] = 2
+        if (is_right_empty(grid, y, x)):
+            y_queue.put(y)
+            x_queue.put(x+1)
+            p_queue.put(path_search+'R')
+            grid[y][x+1] = 2
+        if (is_up_empty(grid, y, x)):
+            y_queue.put(y-1)
+            x_queue.put(x)
+            p_queue.put(path_search+'U')
+            grid[y-1][x] = 2
+        if (is_left_empty(grid, y, x)):
+            y_queue.put(y)
+            x_queue.put(x-1)
+            p_queue.put(path_search+'L')
+            grid[y][x-1] = 2
+    if found:
+        list_path(start, steps, path, final_path)
+    clear_grid(grid)
+
+    if found:
+        print(f"It takes {steps} steps to find a path using BFS")
+    else:
+        print("No path found")
+    return path, steps
+
+
+def dfs(grid, start, goal):
+    '''Return a path found by DFS alogrithm 
+       and the number of steps it takes to find it.
+
+    arguments:
+    grid - A nested list with datatype int. 0 represents free space while 1 is obstacle.
+           e.g. a 3x3 2D map: [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+    start - The start node in the map. e.g. [0, 0]
+    goal -  The goal node in the map. e.g. [2, 2]
+
+    return:
+    path -  A nested list that represents coordinates of each step (including start and goal node), 
+            with data type int. e.g. [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2]]
+    steps - Number of steps it takes to find the final solution, 
+            i.e. the number of nodes visited before finding a path (including start and goal node)
+
+    >>> from main import load_map
+    >>> grid, start, goal = load_map('test_map.csv')
+    >>> dfs_path, dfs_steps = dfs(grid, start, goal)
+    It takes 9 steps to find a path using DFS
+    >>> dfs_path
+    [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2], [2, 3], [3, 3], [3, 2], [3, 1]]
+    '''
+    ### YOUR CODE HERE ###
+    path = []
+    steps = 0
+    found = False
+
+    if found:
+        print(f"It takes {steps} steps to find a path using DFS")
+    else:
+        print("No path found")
+    return path, steps
+
+
+# Doctest
+if __name__ == "__main__":
+    # load doc test
+    from doctest import testmod, run_docstring_examples
+    # Test all the functions
+    testmod()
--- README.md
+++ README.md
@@ -4,17 +4,7 @@
 
 - [Introduction](#introduction)
 - [How to run the Assignments](#how-to-run-the-assignments)
-  - [Assignment 1](#assignment-1)
-  - [Assignment 2](#assignment-2)
-  - [Assignment 3](#assignment-3)
-  - [Assignment 4](#assignment-4)
-  - [Assignment 5](#assignment-5)
-  - [Assignment 6](#assignment-6)
-  - [Assignment 7](#assignment-7)
-  - [Assignment 8](#assignment-8)
-  - [Group Assignment 1](#group-assignment-1)
-  - [Group Assignment 2](#group-assignment-2)
-  - [Group Assignment 3](#group-assignment-3)
+    - [Assignment 1](#assignment-1)
 - [Designer Details](#designer-details)
 - [License](#license)
 
@@ -22,96 +12,21 @@
 
 # Introduction
 
-The aim of the course is to learn Basics of Foundation which includes topics like:
-
-- DH Parameters
-- ROS2 Humble
-- Jacobian
-- Forward and Inverse Kinematics
-- Sensors and Systems
-- Probablistic Robotics
+The aim of the course is to learn and understand Motion Planning for Path planning and Trajectory Generation
 
 Each Assignment shown here has a detailed explaination in its own directory. Please refer the individual directories for in-depth explaination of how the code works.
 
 # How to run the Assignments
 
-## Assignment 1
+## [Assignment 1](/HW%201%3A%20BFS%20and%20DFS%20Algorithm/)
 
-## Assignment 2
-
-## Assignment 3
-
-## Assignment 4
-
-## Assignment 5
-
-## Assignment 6
-
-## Assignment 7
-
-## [Assignment 8](https://github.com/parth-20-07/Foundation-of-Robotics/tree/main/HW%208)
-
-The aim of the assignment is to learn Probabilistic Robotics.
-
-cd to the Directory in terminal
-
-```
-cd HW\ 8/Solution/Code/
-```
-
-Run the program by typing
-
-```
-./main.exe
-```
-
-You should recieve output similar to as shown below
-
-```
-$ ./main.exe
------------------------------------------------------------
-Action: Do Nothing
-Sensor Reading: Door Close
-New Door Open Belief: 0.333333
-New Door Close Belief: 0.666667
-New Normalizer: 1.66667
------------------------------------------------------------
-Action: Open Door
-Sensor Reading: Door Close
-New Door Open Belief: 0.764706
-New Door Close Belief: 0.235294
-New Normalizer: 2.20588
------------------------------------------------------------
-Action: Do Nothing
-Sensor Reading: Door Close
-New Door Open Belief: 0.619048
-New Door Close Belief: 0.380952
-New Normalizer: 2.02381
------------------------------------------------------------
-Action: Open Door
-Sensor Reading: Door Open
-New Door Open Belief: 0.973244
-New Door Close Belief: 0.0267559
-New Normalizer: 1.75585
------------------------------------------------------------
-Action: Do Nothing
-Sensor Reading: Door Open
-New Door Open Belief: 0.990919
-New Door Close Belief: 0.00908059
-New Normalizer: 1.69694
-```
-
-## Group Assignment 1
-
-## Group Assignment 2
-
-## Group Assignment 3
+The aim of the assignment is to learn Breadth-First Search and Depth-First Search implementation.
 
 # Designer Details
 
 - Designed for:
   - Worcester Polytechnic Institute
-  - RBE 500: Foundation of Robotics - Assignments
+  - RBE 550-S02: Motion Planning - Assignments
 - Designed by:
   - [Parth Patel](mailto:parth.pmech@gmail.com)
 
@@ -128,25 +43,3 @@ You may obtain a copy of the License at
 _https://www.gnu.org/licenses/gpl-3.0.en.html_
 
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-
-<!-- TOC -->
-
-Documentation TODO
-
-- [ ] [Introduction](#introduction)
-- [ ] [How to run the Assignments](#how-to-run-the-assignments)
-  - [ ] [Assignment 1](#assignment-1)
-  - [ ] [Assignment 2](#assignment-2)
-  - [ ] [Assignment 3](#assignment-3)
-  - [ ] [Assignment 4](#assignment-4)
-  - [ ] [Assignment 5](#assignment-5)
-  - [ ] [Assignment 6](#assignment-6)
-  - [ ] [Assignment 7](#assignment-7)
-  - [ ] [Assignment 8](#assignment-8)
-  - [ ] [Group Assignment 1](#group-assignment-1)
-  - [ ] [Group Assignment 2](#group-assignment-2)
-  - [ ] [Group Assignment 3](#group-assignment-3)
-- [ ] [Designer Details](#designer-details)
-- [ ] [License](#license)
-
-<!-- /TOC -->
